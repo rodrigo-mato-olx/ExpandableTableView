773e5bf9ed0a1fa97777bb7bc6c1a524dafa3d17
Rodrigo Mato <rodrigo.mato@olx.com>
2014-10-31 15:11:35 -0300

    Initializing the project

---
+++ ExpandableTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpandableTableView
+//
+//  Created by Rodrigo Mato on 10/31/14.
+//  Copyright (c) 2014 Rodrigo Mato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExpandableTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExpandableTableView
+//
+//  Created by Rodrigo Mato on 10/31/14.
+//  Copyright (c) 2014 Rodrigo Mato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
