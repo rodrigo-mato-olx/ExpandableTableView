14a72e8056a8aae4f248e571f28d7fff7e7ce53d
Rodrigo Mato <rodrigo.mato@olx.com>
2014-11-06 16:07:50 -0300

    Finished ExpandableTableController implementation.

--- ExpandableTableView/Controllers/ExpandableTableController.h
+++ ExpandableTableView/Controllers/ExpandableTableController.h
@@ -8,10 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ExpandableTableController : UIViewController <UITableViewDelegate, UITableViewDataSource>
-
-@property (strong, nonatomic) NSMutableArray* tableItems;
+@interface ExpandableTableController : UIViewController <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+- (void)initExpansionStates;
+- (BOOL)isExpansionRow:(NSIndexPath*)indexPath;
+- (NSIndexPath*)absoluteIndexPathFromRelativeIndexPath:(NSIndexPath*)indexPath;
+- (NSInteger)numberOfFixedRowsInSection:(NSInteger)section;
+- (NSInteger)numberOfSections;
 
 @end
--- ExpandableTableView/Controllers/ExpandableTableController.m
+++ ExpandableTableView/Controllers/ExpandableTableController.m
@@ -10,7 +10,7 @@
 
 @interface ExpandableTableController ()
 
-@property (nonatomic, strong) NSMutableSet* expandedRows;
+@property (nonatomic, strong) NSMutableArray* expansionStates;
 
 @end
 
@@ -29,12 +29,145 @@
 - (void)viewDidLoad {
     
     [super viewDidLoad];
-    
-    self.tableItems = [NSMutableArray arrayWithArray:@[ @[@1, @2, @3], @[@4, @5], @[@6, @7, @8] ]];
-    
 }
 
+- (void)initExpansionStates {
+    
+    self.expansionStates = [NSMutableArray new];
+    NSInteger sectionsCount = [self numberOfSections];
+    NSInteger rowsCount;
+    
+    // Create all expansion states in matrix set to NO
+    for (int i=0; i < sectionsCount; i++) {
+        
+        rowsCount = [self numberOfFixedRowsInSection:i];
+        self.expansionStates[i] = [NSMutableArray new];
+        
+        for (int j=0; j < rowsCount; j++)
+            [self.expansionStates[i] addObject:@NO];
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Expanded TableView
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+- (void)expandRow:(NSIndexPath*)indexPath withAbsolutePath:(NSIndexPath*)absolutePath{
+    
+    NSUInteger newPosition = indexPath.row + 1;
+    // Add expanded row to data model
+    NSMutableArray* sectionExpansionRows = self.expansionStates[indexPath.section];
+    [sectionExpansionRows replaceObjectAtIndex:absolutePath.row withObject:@YES];
+    
+    // Insert row in tableview
+    [self.tableView beginUpdates];
+    [self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:newPosition inSection:indexPath.section]] withRowAnimation:UITableViewRowAnimationBottom];
+    [self.tableView endUpdates];
+}
+
+- (void)collapseExpandedRow:(NSIndexPath*)indexPath withAbsolutePath:(NSIndexPath*)absolutePath{
+    
+    NSIndexPath* collapsingIndexPath = [NSIndexPath indexPathForRow:indexPath.row+1 inSection:indexPath.section];
+    // Remove expanded row from data model
+    NSMutableArray* sectionExpansionRows = self.expansionStates[indexPath.section];
+    [sectionExpansionRows replaceObjectAtIndex:absolutePath.row withObject:@NO];
+    
+    // Remove row from tableview
+    [self.tableView beginUpdates];
+    [self.tableView deleteRowsAtIndexPaths:@[collapsingIndexPath] withRowAnimation:UITableViewRowAnimationBottom];
+    [self.tableView endUpdates];
+}
+
+/*
+ ** Indicates if the given row is currently expanded
+ */
+- (BOOL)rowIsExpanded:(NSIndexPath*)indexPath {
+    
+    if (indexPath
+        && (indexPath.section < [_expansionStates count])
+        && (indexPath.row < [_expansionStates[indexPath.section] count]) ) {
+        
+        return [_expansionStates[indexPath.section][indexPath.row] boolValue];
+    }
+    return NO;
+}
+
+/*
+** Indicates if the given row indexpath is an expandable row or an expanded row
+*/
+- (BOOL)isExpansionRow:(NSIndexPath*)indexPath {
+    
+    NSMutableArray* sectionExpansionRows = self.expansionStates[indexPath.section];
+    NSUInteger i = 0;
+    NSInteger realRow = 0;
+    
+    while (i <= indexPath.row) {
+
+        if ( i == indexPath.row )
+            return YES;
+        if ( [sectionExpansionRows[realRow] boolValue] ) {
+            i++;
+            if ( i == indexPath.row )
+                return NO;
+        }
+        i++;
+        realRow++;
+    }
+    return NO;
+}
+
+/*
+** Obtains the real indexpath by substracting the expanded rows
+*/
+- (NSIndexPath*)absoluteIndexPathFromRelativeIndexPath:(NSIndexPath*)indexPath {
+    
+    if ( ![self isExpansionRow:indexPath] )
+        return nil;
+    
+    NSInteger absoluteRow = 0;
+    NSMutableArray* sectionExpandedRows = self.expansionStates[indexPath.section];
+    int i = 0;
+    while (i <= indexPath.row) {
+        
+        if ( i == indexPath.row )
+            break;
+        if ( [sectionExpandedRows[absoluteRow] boolValue] )
+            i++;
+        i++;
+        absoluteRow++;
+    }
+    return [NSIndexPath indexPathForRow:absoluteRow inSection:indexPath.section];
+}
+
+/*
+** Returns the amount of expanded rows for one section
+*/
+- (int)expandedRowsInSection:(NSInteger)section {
+    
+    return [self expandedRowsInSection:section belowRow:[self.expansionStates[section] count]];
+}
+
+/*
+ ** Returns the amount of expanded rows for one section
+ */
+- (int)expandedRowsInSection:(NSInteger)section belowRow:(NSInteger)limitRow{
+    
+    NSArray* sectionStates = self.expansionStates[section];
+    NSInteger limit = limitRow < [sectionStates count]? limitRow : [sectionStates count];
+    int expandedRows = 0;
+    for (int i=0; i < limit; i++)
+        if ([sectionStates[i] boolValue])
+            expandedRows++;
+    return expandedRows;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - Override Class Methods
+/////////////////////////////////////////////////////////////////////////////////////////
+
 
 - (BOOL)shouldExpandRowAtIndexPath:(NSIndexPath*)indexPath {
     
@@ -44,59 +177,40 @@
     return NO;
 }
 
-- (id)expandRowTypeForIndexpath:(NSIndexPath*)indexpath {
+- (NSInteger)numberOfFixedRowsInSection:(NSInteger)section {
     
     @throw [NSException exceptionWithName:NSInternalInconsistencyException
-                                   reason:@"ExpandableTableView - Calling an unimplemented abstract method - expandRowTypeForIndexpath:"
+                                   reason:@"ExpandableTableView - Calling an unimplemented abstract method - numberOfFixedRowsInSection:"
                                  userInfo:nil];
-    return NO;
+    return 0;
+}
+
+- (NSInteger)numberOfSections {
+    
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:@"ExpandableTableView - Calling an unimplemented abstract method - numberOfSections"
+                                 userInfo:nil];
+    return 0;
 }
 
 
+/////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - UITableView Delegate
+/////////////////////////////////////////////////////////////////////////////////////////
+
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-  
-    BOOL rowIsExpanded = [_expandedRows containsObject:indexPath];
     
-    if ( rowIsExpanded ) {
-        // collapse row
-    }
-    else {
+    if ( [self isExpansionRow:indexPath] ) {
         
-        NSUInteger newPosition = indexPath.row + 1;
-        id rowType = [self expandRowTypeForIndexpath:indexPath];
+        NSIndexPath* absolutePath = [self absoluteIndexPathFromRelativeIndexPath:indexPath];
         
-        [self.menuTableView insertRowsAtIndexPaths:arrCells withRowAnimation:UITableViewRowAnimationLeft];
-    }
-    
-    NSDictionary *dic=[self.itemsInTable objectAtIndex:indexPath.row];
-    if([dic valueForKey:@"SubItems"])
-    {
-        NSArray *arr=[dic valueForKey:@"SubItems"];
-        BOOL isTableExpanded=NO;
-        
-        for(NSDictionary *subitems in arr )
-        {
-            NSInteger index=[self.itemsInTable indexOfObjectIdenticalTo:subitems];
-            isTableExpanded=(index>0 && index!=NSIntegerMax);
-            if(isTableExpanded) break;
-        }
-        
-        if(isTableExpanded)
-        {
-            [self CollapseRows:arr];
-        }
-        else
-        {
-            NSUInteger count=indexPath.row+1;
-            NSMutableArray *arrCells=[NSMutableArray array];
-            for(NSDictionary *dInner in arr )
-            {
-                [arrCells addObject:[NSIndexPath indexPathForRow:count inSection:0]];
-                [self.itemsInTable insertObject:dInner atIndex:count++];
-            }
-            [self.menuTableView insertRowsAtIndexPaths:arrCells withRowAnimation:UITableViewRowAnimationLeft];
+        if ( absolutePath && [self shouldExpandRowAtIndexPath:absolutePath] ) {
+            
+            if ( [self rowIsExpanded:absolutePath] )
+                [self collapseExpandedRow:indexPath withAbsolutePath:absolutePath];
+            else
+                [self expandRow:indexPath withAbsolutePath:absolutePath];
         }
     }
 }
@@ -113,16 +227,19 @@
 }
 
 
+/////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - UITableView DataSource
+/////////////////////////////////////////////////////////////////////////////////////////
+
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     
-    return [_tableItems count];
+    return [self numberOfSections];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return [_tableItems[section] count];
+    return [self numberOfFixedRowsInSection:section] + [self expandedRowsInSection:section];
 }
 
 /*
@@ -135,5 +252,23 @@
                                  userInfo:nil];
     return nil;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Textfield Delegate
+/////////////////////////////////////////////////////////////////////////////////////////
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    // On ReturnKey in TextField, hide keyboard
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Textview Delegate
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 @end
